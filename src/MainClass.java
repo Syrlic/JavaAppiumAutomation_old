d89fdcfefcf76f6d8bf564c7f11763e4c7e4305f
olga.klon <oklonina@gmail.com>
2019-07-15 21:50:56 +0400

    ex_2 and ex_3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,16 @@
 public class MainClass {
 
+    private int class_number = 20;
+    private String class_string = "Hello, world";
+
+    public int getClassNumber() {
+        return class_number;
+    }
+
+    public String getClassString() {
+        return class_string;
+    }
+
     public int getLocalNumber(){
         return 14;
     }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,5 +1,9 @@
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+
 
 public class MainClassTest {
 
@@ -9,5 +13,29 @@ public class MainClassTest {
         int expectedNumber = 14;
         Assert.assertTrue("Expected number = "+ expectedNumber+
                 " isn't equal local number = "+ localNumber, expectedNumber == localNumber);
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        int expectedNumber = 45;
+        int class_number = new MainClass().getClassNumber();
+
+        Assert.assertTrue( "Expected number = "+ expectedNumber+
+                " >= class_number = "+class_number, class_number > expectedNumber);
+    }
+
+    @Test
+    public void testGetClassString(){
+        String expectedStr_1 = "Hello";
+        String expectedStr_2 = "hello";
+        String class_string = new MainClass().getClassString();
+
+        if(class_string.contains(expectedStr_1) || class_string.contains(expectedStr_2))
+        Assert.assertTrue(true);
+        else
+        Assert.assertTrue("No matches found", false);
+       // Assert.assertThat("No matches found", class_string, containsString(expectedStr_1));
+       // Assert.assertThat("No matches found", class_string, containsString(expectedStr_2));
+
     }
 }
